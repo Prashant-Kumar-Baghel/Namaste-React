447315f59c41aac8c71c411a03399bf6d0609101
Prashant-Kumar-Baghel <115722509+Prashant-Kumar-Baghel@users.noreply.github.com>
2023-08-19 03:58:37 +0530

    Create App.js

---
+++ Episode 5/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./components/Header";//To use Header file in this file we have to import it and we can also use .js extenstion in last("./components/Header.js") so both way are same.
+import Body from "./components/Body";
+
+
+const AppLayout=()=>{
+     return (
+        <div className="app "> 
+            <Header/>
+            <Body/>
+        </div>
+     )
+}
+const root=ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout/>);//Way to render an React Functional Component.
