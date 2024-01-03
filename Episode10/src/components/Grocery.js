16804e7fbf240bf86ff599c775ddd21d6f346c11
Prashant-Kumar-Baghel <115722509+Prashant-Kumar-Baghel@users.noreply.github.com>
2024-01-03 13:21:39 +0530

    Create Grocery.js

---
+++ Episode10/src/components/Grocery.js
@@ -0,0 +1,10 @@
+//Assume Grocery is big whole component and it has lot of subcomponents and child components.
+const Grocery=()=>{
+    return (
+       <h1>
+        Our grocery online store, and we have lot of child components inside us.
+        </h1>
+        );
+    
+}
+export default Grocery;
