71b1db1b8062db71afc43537bc34f7496ff27f7f
Prashant-Kumar-Baghel <115722509+Prashant-Kumar-Baghel@users.noreply.github.com>
2024-02-19 13:04:21 +0530

    Create constants.js

---
+++ Episode11/src/utils/constants.js
@@ -0,0 +1,12 @@
+//we named it as a small letter because it is not a component.
+//Write varables in capital letter.
+ const CDN_URL="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_366/";
+
+ const LOGO_URL="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTThk6B4F2Q-aQEosit-VHmYfxhiDDAlBNo-JuA8nQN&s";
+
+const MENU_API="https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=30.7042912&lng=76.7104408&restaurantId="
+export {CDN_URL,LOGO_URL,MENU_API}; 
+// https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=30.7042912&lng=76.7104408&restaurantId=49692&catalog_qa=undefined&submitAction=ENTER
+
+   
+
